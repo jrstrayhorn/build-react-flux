7d87b1a284621cb18cf9167cd53e8f87f79f207c
stepforwardtech <jrstrayhorn@gmail.com>
2017-08-15 06:28:31 -0400

    Adding lodash, mock api, starting author page

---
+++ src/components/authors/authorPage.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var React = require('react');
+var AuthorApi = require('../../api/authorApi');
+
+var Authors = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Author</h1>
+            </div>
+        )
+    }
+})
